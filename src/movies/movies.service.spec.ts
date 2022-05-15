2608c39efcc781a7c1b2919116eda1653925de87
Neo <choonghyun95@gmail.com>
2022-05-16 01:41:50 +0900

    UNIT TESTING

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,4 +1,6 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { title } from 'process';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
@@ -14,5 +16,70 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+
+      expect(result).toBeInstanceOf(Array);
+    });
+
+    describe('getOne', () => {
+      it('should return a movie', () => {
+        service.create({
+          title: 'Test Movie',
+          genres: ['test'],
+          year: 2000,
+        });
+        const movie = service.getOne(1);
+        expect(movie).toBeDefined();
+        expect(movie.id).toEqual(1);
+      });
+
+      it('should throw 404 error', () => {
+        try {
+          service.getOne(999);
+        } catch (e) {
+          expect(e).toBeInstanceOf(NotFoundException);
+          expect(e.message).toEqual('Movie with ID 999 not found.');
+        }
+      });
+    });
+
+    describe('deleteOne', () => {
+      // 1. create movie
+      // 2. movie toBeDefined 확인
+      // 3. delete 해서 true가 반환됐나 확인
+      it('deletes a movie', () => {
+        service.create({
+          title: 'Test Movie',
+          genres: ['test'],
+          year: 2000,
+        });
+        const allMovies = service.getAll();
+        service.deleteOne(1);
+        const afterDelete = service.getAll();
+        expect(afterDelete.length).toEqual(allMovies.length - 1);
+      });
+
+      it('should return a 404', () => {
+        try {
+          service.deleteOne(999);
+        } catch (e) {
+          expect(e).toBeInstanceOf(NotFoundException);
+        }
+      });
+    });
+
+    describe('create', () => {
+      it('should create a movie', () => {
+        const beforeCreate = service.getAll().length;
+        service.create({ title: 'Test Movie', genres: ['test'], year: 2000 });
+        const afterCreate = service.getAll().length;
+        console.log(beforeCreate, afterCreate);
+        expect(beforeCreate).toBeLessThan(afterCreate);
+      });
+    });
   });
 });
